3bc7da260402007d20b5bdb991a51e7f79d3fb07
Thorsten Sommer <service@sommer-engineering.com>
2015-08-29 22:07:56 +0200

    Version 1.0.0

---
+++ OceanPDFSearch/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: TSLocal
+ * Date: 29.08.2015
+ * Time: 21:25
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace OceanPDFSearch
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Button buttonSearch;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ListBox listBoxResults;
+		private System.Windows.Forms.Button buttonIndex;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.buttonSearch = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.listBoxResults = new System.Windows.Forms.ListBox();
+			this.buttonIndex = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 4;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 77F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 79F));
+			this.tableLayoutPanel1.Controls.Add(this.buttonSearch, 3, 0);
+			this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.listBoxResults, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.buttonIndex, 0, 2);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(873, 540);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// buttonSearch
+			// 
+			this.buttonSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.buttonSearch.Location = new System.Drawing.Point(796, 3);
+			this.buttonSearch.Name = "buttonSearch";
+			this.buttonSearch.Size = new System.Drawing.Size(74, 29);
+			this.buttonSearch.TabIndex = 0;
+			this.buttonSearch.Text = "Search";
+			this.buttonSearch.UseVisualStyleBackColor = true;
+			this.buttonSearch.Click += new System.EventHandler(this.ButtonSearchClick);
+			// 
+			// textBox1
+			// 
+			this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 3);
+			this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBox1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(3, 3);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(787, 29);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextBox1KeyUp);
+			// 
+			// listBoxResults
+			// 
+			this.tableLayoutPanel1.SetColumnSpan(this.listBoxResults, 2);
+			this.listBoxResults.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listBoxResults.FormattingEnabled = true;
+			this.listBoxResults.HorizontalScrollbar = true;
+			this.listBoxResults.ItemHeight = 18;
+			this.listBoxResults.Location = new System.Drawing.Point(3, 38);
+			this.listBoxResults.Name = "listBoxResults";
+			this.listBoxResults.Size = new System.Drawing.Size(307, 464);
+			this.listBoxResults.TabIndex = 2;
+			this.listBoxResults.SelectedValueChanged += new System.EventHandler(this.ListBoxResultsSelectedValueChanged);
+			// 
+			// buttonIndex
+			// 
+			this.buttonIndex.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.buttonIndex.Location = new System.Drawing.Point(3, 508);
+			this.buttonIndex.Name = "buttonIndex";
+			this.buttonIndex.Size = new System.Drawing.Size(71, 29);
+			this.buttonIndex.TabIndex = 3;
+			this.buttonIndex.Text = "Index";
+			this.buttonIndex.UseVisualStyleBackColor = true;
+			this.buttonIndex.Click += new System.EventHandler(this.ButtonIndexClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(873, 540);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Ocean PDF Search";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ OceanPDFSearch/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+using OceanPDFSearchLib;
+using NETools;
+
+namespace OceanPDFSearch
+{
+	public partial class MainForm : Form
+	{
+		private string workingDirectory = Environment.CurrentDirectory;
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			this.Text = string.Format("Ocean PDF Search v{0}@v{1}", Assembly.GetExecutingAssembly().getAppVersion(), Assembly.GetAssembly(OceanSearchManager.INSTANCE.GetType()).getAppVersion());
+			OceanSearchManager.INSTANCE.Setup(this.workingDirectory);
+		}
+		
+		void TextBox1KeyUp(object sender, KeyEventArgs e)
+		{
+			if(e.KeyCode == Keys.Enter)
+			{
+				this.ButtonSearchClick(sender, e);
+			}
+		}
+		
+		void ButtonSearchClick(object sender, EventArgs e)
+		{
+			var searchFor = this.textBox1.Text;
+			var result = new string[0];
+			
+			if (searchFor.Contains(">"))
+			{
+				var elements = searchFor.Split('<', '>');
+				var word1 = elements[0].Trim();
+				var word2 = elements[2].Trim();
+				var distance = int.Parse(elements[1].Trim());
+				result = OceanSearchManager.INSTANCE.FindDocumentsProximitySearch(word1, word2, distance);
+			}
+			else
+			{
+				result = OceanSearchManager.INSTANCE.FindDocumentsContainAllWords(searchFor.Split(' '));
+			}
+			
+			this.listBoxResults.Items.Clear();
+			this.listBoxResults.Items.AddRange(result);
+		}
+		
+		void ButtonIndexClick(object sender, EventArgs e)
+		{
+			//OceanSearchManager.INSTANCE.IndexNow();
+		}
+		
+		void ListBoxResultsSelectedValueChanged(object sender, EventArgs e)
+		{
+			var selectedFile = this.listBoxResults.SelectedItem as string;
+			Process.Start(@"c:\Program Files\Tracker Software\PDF Editor\PDFXEdit.exe", string.Format(@"/A search=""{1}"" ""{0}""", selectedFile, this.textBox1.Text));
+		}
+	}
+}
